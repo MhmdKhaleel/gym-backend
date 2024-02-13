0181c44e35f8557bc84023a2f8a5c43b14eeb071
MhmdKhaleel <khaleel3936@gmail.com>
2024-02-13 15:19:55 +0530

    delete

--- controllers/gymrouter.js
+++ controllers/gymrouter.js
@@ -20,4 +20,10 @@ router.post('/search',async(req,res)=>{
     let data=await gymmodel.find(input)
     res.json(data)
 })
+
+router.post('/delete',async(req,res)=>{
+    let input=req.body
+    let response=await gymmodel.deleteOne(input)
+    res.json({"status":"success"})
+})
 module.exports=router
